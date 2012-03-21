dfc62d8401a6aa55178460c319021381750f3db0
Adam Clarke <aclarke@iseatz.com>
2012-03-21 00:39:38 -0500

    think i fixed a sorting issue

--- app.rb
+++ app.rb
@@ -70,6 +70,6 @@ post '/battle' do
 end
 
 get '/leaderboard' do
-  @haikus = Haiku.all.sort_by { |haiku| haiku.wins.count }
+  @haikus = Haiku.all.sort_by { |haiku| haiku.wins.length }
   haml :leaderboard
 end
